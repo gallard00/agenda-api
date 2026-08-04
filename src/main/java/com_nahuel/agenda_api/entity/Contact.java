550c5fd7c57ce81fdaad59ff5629680bd0e6a7e5
Nahuel Gallardo <nahuel.ng240@gmail.com>
2026-08-04 20:29:49 -0300

    refactor: add annotation builder

--- src/main/java/com_nahuel/agenda_api/entity/Contact.java
+++ src/main/java/com_nahuel/agenda_api/entity/Contact.java
@@ -1,10 +1,7 @@
 package com_nahuel.agenda_api.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -12,6 +9,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 @Table(name = "contact")
 public class Contact {
 
